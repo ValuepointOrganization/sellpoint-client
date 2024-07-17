b653b0c7f2726d285fc88644a4a19e25ce3b03d6
NakedGreen <jokemup@gmail.com>
2024-07-17 19:47:37 +0900

    Removed the "Welcome to SellPoint" heading from the main page.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import Header from '../layout/Header';
+import React from "react";
+import Header from "../components/layout/Header";
 
 const MainPage = () => {
   return (
     <div>
       <Header />
-      <h1>Welcome to SellPoint</h1>
-      {/* Add more content for the main page here */}
     </div>
   );
 };
