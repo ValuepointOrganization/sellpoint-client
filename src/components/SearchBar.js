aaac735bdbdde8d84bd46e4e58bba3d1077f31f0
NakedGreen <jokemup@gmail.com>
2024-07-18 00:02:18 +0900

    Implemented a new background color for the SearchBar component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const SearchBarContainer = styled.div`
   display: flex;
   align-items: center;
   padding: 10px;
-  background-color: #f1f1f1;
+  background-color: #bababf;
   border-radius: 25px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
