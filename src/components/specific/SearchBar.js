cd6fc6ac531b66170e1a1310ca515019c9df5a39
NakedGreen <jokemup@gmail.com>
2024-07-21 03:09:33 +0900

    Added a new SearchBar component in the src/components/specific directory.

---
+++ src/components/specific/SearchBar.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIconSvg from "../../assets/image/search.svg";
+import CloseIconSvg from "../../assets/image/close.svg";
+
+const SearchBar = () => {
+  return (
+    <SearchBarContainer>
+      <SearchIcon src={SearchIconSvg} alt="Search" />
+      <SearchInput placeholder="종목 또는 애널리스트를 검색하세요." />
+      <ClearIcon src={CloseIconSvg} alt="Clear" />
+    </SearchBarContainer>
+  );
+};
+
+export default SearchBar;
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  padding: 12px;
+  justify-content: space-between;
+  width: 100%;
+  height: 50px;
+  align-items: center;
+  align-self: stretch;
+  border-radius: 24px;
+  background: #f5f6f7;
+  backdrop-filter: blur(50px);
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  background: none;
+  flex: 1;
+  font-size: 16px;
+`;
+
+const SearchIcon = styled.img`
+  margin-right: 12px;
+  width: 18px;
+  height: 18px;
+`;
+
+const ClearIcon = styled.img`
+  margin-left: 12px;
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+`;
