5175a4c5b05bda5c30bfe50b4cadd129d80bc7b2
NakedGreen <jokemup@gmail.com>
2024-07-20 15:03:16 +0900

    Added a new array of dummy analyst data to the dummy.js file.

--- src/assets/dummy.js
+++ src/assets/dummy.js
@@ -17,3 +17,50 @@ const DummyStock = [
 ];
 
 export default DummyStock;
+
+const DummyAnalyst = [
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+  {
+    name: "한유진",
+    firm: "한국투자",
+  },
+];
