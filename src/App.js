cf06ce2e091c865ddcaef4cb23f517e23b05b62e
NakedGreen <jokemup@gmail.com>
2024-07-21 04:43:54 +0900

    Added new routes for AnalystList and AnalystDetail pages.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./assets/styles/globalStyle";
-import MainPage from './pages/main';
-
+import Main from "./pages/Main";
+import AnalystList from "./pages/AnalystList";
+import AnalystDetail from "./pages/AnalystDetail";
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <div className="App">
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<Main />} />
+          <Route path="/analyst-list" element={<AnalystList />} />
+          <Route path="/analyst/:i" element={<AnalystDetail />} />
           {/* Add more routes here as needed */}
         </Routes>
       </div>
