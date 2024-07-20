aba44ae6782f765b0743c110f496bb7b34eec378
NakedGreen <jokemup@gmail.com>
2024-07-20 14:09:30 +0900

    Added dummy asset file and main stock list component.

---
+++ src/assets/dummy.js
@@ -0,0 +1,17 @@
+const DummyStock = [
+  {
+    code: "NVDA",
+    up: "+5.63",
+    name: "Nvidia",
+  },
+  {
+    code: "NVDA",
+    up: "+5.63",
+    name: "Nvidia",
+  },
+  {
+    code: "NVDA",
+    up: "+5.63",
+    name: "Nvidia",
+  },
+];
---
+++ src/components/specific/Main/MainStockList.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import MoreButton from "./MoreButton";
+import Space from "../../common/Space";
+
+import NvidiaLogo from "../../../assets/image/nvidia.svg";
+import DummyStock from "../../../assets/dummy.js";
+
+const MainStockList = () => {
+  console.log(DummyStock);
+  return (
+    <StockListContainer>
+      <ListHeader>
+        <span style={{ fontSize: "16px", fontWeight: 600, lineHeight: "160%" }}>
+          인기 종목
+        </span>
+        <MoreButton text="종목 더보기" width={82} height={22} />
+      </ListHeader>
+      <Space height="20px" />
+      <ListSquare>
+        <StockProfile>
+          <img src={NvidiaLogo} alt="Nvidia Logo" />
+          {/* <span>{DummyStock[0]["name"]}</span> */}
+        </StockProfile>
+        <StockInfo></StockInfo>
+      </ListSquare>
+    </StockListContainer>
+  );
+};
+
+export default MainStockList;
+
+const StockListContainer = styled.div`
+  width: 100%;
+`;
+
+const ListHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`;
+
+const ListSquare = styled.div`
+  padding: 12px;
+  border-radius: 8px;
+  background: #f5f6f7;
+`;
+
+const StockProfile = styled.div`
+  display: flex;
+  align-items: center;
+  gpa-12px;
+`;
+
+const StockInfo = styled.div`
+  display: flex;
+  width: 33px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 4px;
+`;
