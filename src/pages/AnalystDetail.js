f7b7b25966740f67bc7d4f8ec0874925bbc59da5
NakedGreen <jokemup@gmail.com>
2024-07-22 14:00:21 +0900

    Added a vertical space component to the AnalystDetail page.

--- src/pages/AnalystDetail.js
+++ src/pages/AnalystDetail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/layout/Header";
 import AnalystDetailProfile from "../components/layout/AnalystDetail/AnalystDetailProfile";
+import Space from "../components/common/Space.js";
 import { DummyAnalystProfile } from "../assets/dummy";
 
 const AnalystDetail = () => {
@@ -8,6 +9,7 @@ const AnalystDetail = () => {
     <>
       <Header />
       <AnalystDetailProfile analystProfile={DummyAnalystProfile[0]} />
+      <Space height="32px" />
     </>
   );
 };
