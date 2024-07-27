c5f90361b30a841e99b8c5d9a9d97560e77a8692
NakedGreen <jokemup@gmail.com>
2024-07-28 01:49:02 +0900

    Added a new component for an Agree/Disagree button in the AnalystReport section.

---
+++ src/components/specific/AnalystReport/AgreeDisAgreeButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Flex } from "../../common/Index";
+
+const AgreeDisAgreeButton = () => {
+  return <Flex flexDirection="column" alignItems="center" gap="8px"></Flex>;
+};
+
+export default AgreeDisAgreeButton;
