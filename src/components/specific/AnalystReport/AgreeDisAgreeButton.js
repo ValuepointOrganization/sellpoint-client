0734bcd1babf13488b979eb6f3fe8569ce3d1809
NakedGreen <jokemup@gmail.com>
2024-07-28 02:00:14 +0900

    Refactored the AgreeDisAgreeButton component to use children prop instead of text, and adjusted the styling of the button.

--- src/components/specific/AnalystReport/AgreeDisAgreeButton.js
+++ src/components/specific/AnalystReport/AgreeDisAgreeButton.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Flex, Image, Text } from "../../common/Index";
 
-const AgreeDisAgreeButton = ({ image, text, number }) => {
+const AgreeDisAgreeButton = ({ image, children, number }) => {
   return (
     <Flex flexDirection="column" alignItems="center" gap="8px">
-      <Image src={image} alt={text} width="24px" height="24px" />
-      <Text>{text}</Text>
-      <Text>{number}</Text>
+      <Image src={image} alt={children} width="18px" height="18px" />
+      <Text
+        fontSize="12px"
+        fontStyle="normal"
+        fontWeight="600"
+        lineHeight="22px"
+        color="#D5D5D5"
+      >
+        {children}
+      </Text>
+      <Text
+        fontSize="12px"
+        fontStyle="normal"
+        fontWeight="600"
+        lineHeight="22px"
+        color="#D5D5D5"
+      >
+        {number}
+      </Text>
     </Flex>
   );
 };
