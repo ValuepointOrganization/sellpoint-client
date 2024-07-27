ea06827aa7303a14204bf2b2e916b589979400a8
NakedGreen <jokemup@gmail.com>
2024-07-28 04:23:17 +0900

    Added a new Comment component and integrated it into the AnalystReportBodyComment component.

--- src/components/layout/AnalystReport/AnalystReportBodyComment.js
+++ src/components/layout/AnalystReport/AnalystReportBodyComment.js
@@ -2,6 +2,14 @@ import { React } from "react";
 import { styled } from "styled-components";
 
 import { Flex, Text } from "../../common/Index";
+import Comment from "../../specific/AnalystReport/Comment";
+
+const dummy = {
+  userName: "Jake",
+  content:
+    "별 하나에 추억과 별 하나에 사랑과 별 하나에 쓸쓸함과 별 하나에 동경과 별 하나에 시와 별 하나에 어머니, 어머니, 어머님, 나는 별 하나에 아름다운 말 한 마디씩 불러 봅니다.",
+  dateTime: "04/28/2024 20:27",
+};
 
 const AnalystReportBodyComment = () => {
   return (
@@ -12,14 +20,20 @@ const AnalystReportBodyComment = () => {
       gap="24px"
       style={{ alignSelf: "stretch" }}
     >
-      <Flex direction="row" justify="space-between" align="center" style={{ width: "100%" }}>
+      <Flex
+        direction="row"
+        justify="flex-start"
+        align="center"
+        style={{ width: "100%" }}
+      >
         <Text fontSize="16px" lineHeight="22px">
-          이 리포트에 대한 댓글
+          이 리포트에 대한 댓글&nbsp;
         </Text>
         <Text color="#8c8c8c" fontSize="16px" lineHeight="22px">
           24
         </Text>
       </Flex>
+      <Comment props={dummy} />
     </Flex>
   );
 };
--- src/components/specific/AnalystReport/Comment.js
+++ src/components/specific/AnalystReport/Comment.js
@@ -1,14 +1,21 @@
 import { React } from "react";
 import { styled } from "styled-components";
 
-import { Flex } from "../../common/Index";
+import { Flex, Text } from "../../common/Index";
+import StyledSVG from "../../common/StyledSVG";
+
 import { ReactComponent as PersonIcon } from "../../../assets/image/PersonIcon.svg";
 
-const Comment = ({ userName, content, dateTime }) => {
+const Comment = ({ prpops }) => {
   return (
     <Flex direction="column" justify="center" align="flex-start" gap="12px">
       <Flex align="center" gap="8px">
-        <PersonIcon width="20" height="21" fill="#DEDEDF" />
+        <StyledSVG width="20" height="21" viewBox="0 0 20 21" fill="#DEDEDF">
+          <PersonIcon />
+        </StyledSVG>
+        <Text fontSize="13px" lineHeight="160%">
+          {prpops.userName}
+        </Text>
       </Flex>
     </Flex>
   );
