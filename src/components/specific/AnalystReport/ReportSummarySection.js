74d66f03fb59be7a0427a7430d71ba27a9d65e15
NakedGreen <jokemup@gmail.com>
2024-07-27 21:55:13 +0900

    Added Space component and adjusted list styling in ReportSummarySection.

--- src/components/specific/AnalystReport/ReportSummarySection.js
+++ src/components/specific/AnalystReport/ReportSummarySection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
 
-import { Text, Flex } from "../../common/Index";
+import { Text, Flex, Space } from "../../common/Index";
 
 const dummySummary = [
   `세계적인 칩 부족 현상으로 인한 공급망 지연: Nvidia는 최근에 세계적인 칩 부족 현상에 직면하고 있으며, 이는 다양한 제품의 공급을 늦추고 있습니다. 이러한 지연으로 인해 수익성과 시장 점유율에 부정적인 영향을 미칠 수 있습니다.`,
@@ -9,19 +9,24 @@ const dummySummary = [
 ];
 const ReportSummarySection = () => {
   return (
-    <ul>
-      {dummySummary.map((report, index) => {
-        return (
-          <li key={`Summary ${index}`}>
-            <Flex align="flex-start" style={{ maxWidth: "100%" }}>
-              <Text fontSize="14px" fontWeight="400" fontStyle="normal">
-                {report}
-              </Text>
-            </Flex>
-          </li>
-        );
-      })}
-    </ul>
+    <Flex>
+      <ul style={{ listStylePosition: "inside" }}>
+        {dummySummary.map((report, index) => {
+          return (
+            <>
+              <li key={`Summary ${index}`}>
+                <Flex align="flex-start" style={{ maxWidth: "100%" }}>
+                  <Text fontSize="14px" fontWeight="400" fontStyle="normal">
+                    {report}
+                  </Text>
+                </Flex>
+              </li>
+              <Space height="14px" />
+            </>
+          );
+        })}
+      </ul>
+    </Flex>
   );
 };
 
