eff5e9deef71945ee1b9205009a5fb2ea79b07a6
NakedGreen <jokemup@gmail.com>
2024-07-28 04:14:52 +0900

    Added a new component for displaying comments in the AnalystReport.

---
+++ src/components/specific/AnalystReport/Comment.js
@@ -0,0 +1,21 @@
+import { React } from "react";
+import { styled } from "styled-components";
+
+import { Flex } from "../../common/Index";
+import StyledSVG from "../../common/StyledSVG";
+
+import { Reactcomponent as PersonIcon } from "../../../assets/image/PersonIcon.svg";
+
+const Comment = ({ userName, content, dateTime }) => {
+  return (
+    <Flex direction="column" justify="center" align="flex-start" gap="12px">
+      <Flex align="center" gap="8px">
+        <StyledSVG width="20" height="21" viewBox="0 0 20 21">
+          <PersonIcon />
+        </StyledSVG>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Comment;
