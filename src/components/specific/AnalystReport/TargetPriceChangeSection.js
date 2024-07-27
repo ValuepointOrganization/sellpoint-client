c9780e9753474b57d7456f4d7e2b0c1aa33d0ad9
NakedGreen <jokemup@gmail.com>
2024-07-27 21:04:15 +0900

    Updated TargetPriceChangeSection to use Space component for spacing instead of Flex.

--- src/components/specific/AnalystReport/TargetPriceChangeSection.js
+++ src/components/specific/AnalystReport/TargetPriceChangeSection.js
@@ -3,18 +3,22 @@ import BarChart from "../../../assets/image/BarChart.svg";
 import InformationCircle from "../../../assets/image/InformationCircle.svg";
 import RightArrow from "../../../assets/image/RightArrow.svg";
 
-import { Text, Image, Flex } from "../../common/index";
+import { Text, Image, Flex, Space } from "../../common/Index";
 
 const TargetPriceChange = () => {
   return (
-    <Flex justify="space-between" align="center" style={{ alignSelf: 'stretch' }}>
+    <Flex
+      justify="space-between"
+      align="center"
+      style={{ alignSelf: "stretch" }}
+    >
       <Flex align="center">
         <Image height="18px" width="18px" src={BarChart} alt="BarChart" />
-        <Flex width="12px" />
+        <Space width="12px" />
         <Text color="#8c8c8c" fontWeight="600" fontSize="14px">
           목표가 상향
         </Text>
-        <Flex width="6px" />
+        <Space width="6px" />
         <Image
           height="12px"
           width="12px"
