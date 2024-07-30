5aa11496f4776e610b013d3be9d20b32077f34e5
NakedGreen <jokemup@gmail.com>
2024-07-30 17:14:53 +0900

    Removed unnecessary comments from stock chart data.

--- src/components/specific/StockDetail/StockDetailStockChart.js
+++ src/components/specific/StockDetail/StockDetailStockChart.js
@@ -23,17 +23,15 @@ const StockDetailStockChart = () => {
       {
         name: "AAPL",
         data: [
-          // Each element is [timestamp, price]
-          // timestamp is milliseconds since Unix epoch
-          [1167609600000, 12.5],  // January 1, 2007
+          [1167609600000, 12.5], // January 1, 2007
           [1167696000000, 12.61], // January 2, 2007
           [1167782400000, 12.69], // January 3, 2007
           [1167868800000, 12.88], // January 4, 2007
           [1167955200000, 12.79], // January 5, 2007
-          [1168214400000, 12.3],  // January 8, 2007
-          [1168300800000, 12.8],  // January 9, 2007
-          [1168387200000, 13.1],  // January 10, 2007
-          [1168473600000, 13.5],  // January 11, 2007
+          [1168214400000, 12.3], // January 8, 2007
+          [1168300800000, 12.8], // January 9, 2007
+          [1168387200000, 13.1], // January 10, 2007
+          [1168473600000, 13.5], // January 11, 2007
           [1168560000000, 13.87], // January 12, 2007
         ],
         tooltip: {
