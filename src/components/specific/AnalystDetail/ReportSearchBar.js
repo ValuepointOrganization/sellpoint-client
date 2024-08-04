ebc33b1c58584b682170f024a3efb46cae777531
NakedGreen <jokemup@gmail.com>
2024-08-04 16:20:50 +0900

    feat: Refactor import path for Flex component

--- src/components/specific/AnalystDetail/ReportSearchBar.js
+++ src/components/specific/AnalystDetail/ReportSearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Flex } from "../../common";
+import { Flex } from "../../common/Index";
 import SearchBar from "../SearchBar";
 import { ReactComponent as FilterIcon } from "../../../assets/image/FilterIcon.svg";
 
