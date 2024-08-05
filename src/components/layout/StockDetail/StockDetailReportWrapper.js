39b39d584958bfb5a8bb61bdccfb40c272609c08
NakedGreen <jokemup@gmail.com>
2024-08-05 17:49:52 +0900

    feat: Add ReportSearchBar component to StockDetailReportWrapper

--- src/components/layout/StockDetail/StockDetailReportWrapper.js
+++ src/components/layout/StockDetail/StockDetailReportWrapper.js
@@ -3,20 +3,38 @@ import { Flex } from "../../common/Index";
 
 import StockDetailReportSelect from "../../specific/StockDetail/StockDetailReportSelect";
 import StockDetailReportBrief from "../../specific/StockDetail/StockDetailReportBrief";
-
+import ReportSearchBar from "../../specific/ReportSearchBar";
 const StockDetailReportWrapper = () => {
   const [reportType, setReportType] = useState("analyst");
 
   return (
-    <Flex
-      align="flex-start"
-      gap="20px"
-      direction="column"
-      style={{ padding: "0px 20px 100px 20px" }}
-    >
-      <StockDetailReportSelect reportType={reportType} setReportType={setReportType} />
-      <StockDetailReportBrief reportType={reportType} />
-    </Flex>
+    <React.Fragment>
+      <Flex
+        style={{
+          padding: "0px 20px 0px 20px",
+        }}
+      >
+        <StockDetailReportSelect
+          reportType={reportType}
+          setReportType={setReportType}
+        />
+      </Flex>
+      <Flex
+        align="flex-start"
+        gap="20px"
+        direction="column"
+        style={{
+          padding: "0px 20px 100px 20px",
+          background: "#FAFAFA;",
+        }}
+      >
+        <StockDetailReportBrief reportType={reportType} />
+        <ReportSearchBar
+          placeholder="애널리스트를 검색하세요."
+          style={{ border: "1px #E6E9ED" }}
+        />
+      </Flex>
+    </React.Fragment>
   );
 };
 
