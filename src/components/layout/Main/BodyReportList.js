cd3a26e29b4f285ab082c0a37543f7aa067ccd24
NakedGreen <jokemup@gmail.com>
2024-09-01 20:02:54 +0900

    feat: Fetch report data from API and display in the report list

--- src/components/layout/Main/BodyReportList.js
+++ src/components/layout/Main/BodyReportList.js
@@ -4,8 +4,26 @@ import ListContainerHeader from "../../specific/Main/ListContainerHeader";
 import Space from "../../common/Space";
 import ReportItem from "../../specific/Main/ReportItem";
 import { DummyReport } from "../../../assets/dummy";
+import axios from "axios";
 
 const BodyReportList = () => {
+  const [reportData, setReportData] = React.useState([]);
+  const [error, setError] = React.useState(null);
+  React.useEffect(() => {
+    const fetchReport = async () => {
+      try {
+        const response = await axios.get(
+          `https://port-0-server-lzz7360l6d1cd162.sel4.cloudtype.app/api/report/analyst?`
+        );
+        setReportData(response.data);
+      } catch (err) {
+        setError("Failed to fetch report data");
+        console.log(error);
+      }
+    };
+    fetchReport();
+  }, []);
+  console.log(reportData);
   return (
     <ReportListContainer>
       <ListContainerHeader
@@ -15,7 +33,7 @@ const BodyReportList = () => {
         buttonHeight={22}
       />
       <Space height="20px" />
-      {DummyReport.map((report, index) => {
+      {reportData.map((report, index) => {
         return <ReportItem report={report} key={index} />;
       })}
     </ReportListContainer>
