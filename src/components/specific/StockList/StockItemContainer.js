6522b2047c86d6f82dcab3ff504b29beade2654c
NakedGreen <jokemup@gmail.com>
2024-11-25 20:55:12 +0900

    [feat] stock list에 navigate 추가

--- src/components/specific/StockList/StockItemContainer.js
+++ src/components/specific/StockList/StockItemContainer.js
@@ -3,19 +3,19 @@ import React from "react";
 import { Flex, Text } from "../../common/Index";
 import { ReactComponent as PriceDownArrow } from "../../../assets/image/PriceDownArrow.svg";
 import { ReactComponent as PriceUpArrow } from "../../../assets/image/PriceUpArrow.svg";
-import { styled } from "styled-components";
 import { formatNumberString } from "../../../utils/stringUtils";
-
+import { useNavigate } from "react-router-dom";
 const StockItemContainer = ({ stock }) => {
+  const navigate = useNavigate();
   console.log(stock);
   const isPriceUp = stock.FLUC_RATE >= 0;
   const PriceChangeIcon = isPriceUp ? PriceUpArrow : PriceDownArrow;
   const priceColor = isPriceUp ? "#FF3B30" : "#3182F6";
   const priceText = formatNumberString(stock.PRICE);
-  console.log(priceText);
   const flucText = isPriceUp
     ? stock.FLUC_RATE
     : stock.FLUC_RATE.replace(/-/g, "");
+
   return (
     <Flex
       justifyContent="space-between"
@@ -23,7 +23,13 @@ const StockItemContainer = ({ stock }) => {
       style={{ padding: "12px 0px", width: "100%", height: "46px" }}
     >
       <Flex direction="column" gap="4px" style={{ width: "100%" }}>
-        <Text color="#000" fontSize="16px" fontWeight="500" lineHeight="140%">
+        <Text
+          color="#000"
+          fontSize="16px"
+          fontWeight="500"
+          lineHeight="140%"
+          onClick={() => navigate(`/stock/${stock.STOCK_ID}`)}
+        >
           {stock.STOCK_NAME}
         </Text>
         <Text
